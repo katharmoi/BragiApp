2ecf031b9f0cde72544ccd0b41e269c93a9613d8
Kadir Kertis <kadir@mamboemm.com>
2025-05-17 02:16:30 +0300

    Add navigation to MainActivity

--- app/src/main/java/com/test/bragiapp/MainActivity.kt
+++ app/src/main/java/com/test/bragiapp/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.test.bragiapp.presentation.navigation.AppNavigation
 import com.test.bragiapp.ui.theme.BragiAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +20,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BragiAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
